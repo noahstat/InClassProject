c54997af95ea36b462a849972baf244d4c204c53
Noah Stasyszen <stasyszen@gmail.com>
2019-10-16 13:30:40 -0500

    implemented the compareTo method and its test

--- Student.java
+++ Student.java
@@ -17,10 +17,29 @@ public class Student extends Person implements Comparable<Student> {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public Student(String firstName, String lastName, Integer rating) throws NameException
+	{
+		super(firstName, lastName);
+		this.rating = rating;
+	}
+	
 	@Override
 	public int compareTo(Student o) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		int comparison = rating - o.getRating();
+		if(comparison < 0)
+		{
+			return -1;
+		}
+		else if(comparison > 0)
+		{
+			return 1;
+		}
+		else 
+		{
+			return 0;
+		}
+		
 	}
 	
 	public Integer getRating()
@@ -28,4 +47,8 @@ public class Student extends Person implements Comparable<Student> {
 		return new Integer(rating);
 	}
 	
+	public void setRating(Integer newRating)
+	{
+		rating = newRating;
+	}
 }
--- StudentTest.java
+++ StudentTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class StudentTest {
-	
 
 	private static final int INITIAL_RATING = 5;
 
@@ -21,15 +20,60 @@ public class StudentTest {
 			s = new Student();
 			rating = s.getRating();
 			Integer expected = INITIAL_RATING;
-			assertEquals(expected, rating);	
+			assertEquals(expected, rating);
 		} catch (NameException e) {
 			fail();
-		}	
+		}
+	}
+
+	@Test
+	public void testSetRating() {
+
+	}
+
+	@Test
+	public void testGetRating() {
+		try {
+			Integer expected = 4;
+			Integer rating = expected;
+			String firstName = "Alex";
+			String lastName = "Bo";
+
+			Student s = new Student(firstName, lastName, rating);
+			Integer actual = s.getRating();
+			assertEquals(expected, actual);
+
+		} catch (NameException e) {
+			fail();
+		}
+
 	}
 
 	@Test
 	public void testCompareTo() {
-		fail("Not yet implemented");
+		try {
+			Student firstStudent = new Student("X", "Y", 2);
+			Student secondStudent = new Student("X", "Y", 3);
+			
+			int actual = secondStudent.compareTo(firstStudent);
+			int expected = 1;
+			
+			assertEquals(expected, actual);
+			
+			Student tudent = new Student("X", "Y", 5);
+			
+			actual = secondStudent.compareTo(tudent);
+			expected = -1;
+			
+			assertEquals(expected, actual);
+			
+		} catch (NameException e) {
+			fail();
+		}
+		
+		
+		
+		
 	}
 
 }
